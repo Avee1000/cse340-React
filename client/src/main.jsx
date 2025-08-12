e8fdf9710e7aea2608d2f4141d33bddd1ca8f543
Avee1000 <eddyidahosa01@gmail.com>
2025-08-12 21:08:42 +0100

    xf

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,13 @@
+// client/src/main.jsx
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 );
